a983e65813c4df82211738a173e60fcf66175682
Alexandr Vagner <sanyapalmero@gmail.com>
2021-07-02 21:56:21 +0500

    Add OfferPrice model

--- parsers/models.py
+++ parsers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Provider(models.Model):
@@ -25,8 +26,30 @@ class Offer(models.Model):
     resource_code = models.CharField(null=True, blank=False, max_length=255, verbose_name="Код строительного ресурса")
     name = models.CharField(null=True, blank=False, max_length=255, verbose_name="Наименование")
     measure_unit = models.CharField(null=True, blank=False, max_length=255, verbose_name="Единица измерения")
-    last_updated = models.DateField(null=True, blank=False, verbose_name="Дата последнего обновления")
+    last_updated = models.DateTimeField(null=True, blank=False, verbose_name="Дата последнего обновления")
     page_url = models.URLField(null=True, blank=False, verbose_name="Ссылка на страницу")
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, verbose_name="Поставщик")
     image_url = models.URLField(null=True, blank=False, verbose_name="Ссылка на картинку")
-    status = models.IntegerField(choices=Status.choices, db_index=True, verbose_name='статус')
+    status = models.IntegerField(choices=Status.choices, db_index=True, verbose_name="Статус")
+
+    @property
+    def last_offer_price(self):
+        return OfferPrice.objects.filter(offer=self).order_by("extraction_date").last()
+
+
+class OfferPrice(models.Model):
+    """
+    Модель: Цена предложегния в определенный момент времени
+    """
+
+    class Status(models.IntegerChoices):
+        UNPUBLISHED = 1, 'Неопубликован'
+        WAITING = 2, 'Ожидает подтверждения'
+        PUBLISHED = 3, 'Опубликован'
+
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, verbose_name="Оффер")
+    price_with_vat = models.CharField(max_length=255, verbose_name="Отпускная цена с НДС")
+    price_without_vat = models.CharField(max_length=255, verbose_name="Стоимость доставки")
+    delivery_cost = models.CharField(max_length=255, verbose_name="Наименование")
+    extraction_date = models.DateTimeField(null=True, blank=False, verbose_name="Дата извлечения")
+    status = models.IntegerField(choices=Status.choices, db_index=True, verbose_name="Статус")
