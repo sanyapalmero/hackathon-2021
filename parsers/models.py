65f425759aa0efcf1f5f84128bc4c3989ad7469c
Alexandr Vagner <sanyapalmero@gmail.com>
2021-07-02 21:23:04 +0500

    Add Provider model

--- parsers/models.py
+++ parsers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Provider(models.Model):
+    """
+    Модель: Поставщик
+    """
+
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    inn = models.CharField(max_length=255, verbose_name="ИНН")
+    kpp = models.CharField(max_length=255, verbose_name="КПП")
+    warehouse_location = models.CharField(max_length=255, verbose_name="Населенный пункт склада")
