e021007a32437ea4b239227a9d117537f4da6ba3
Oleg Yamnikov <oleg@oyam.dev>
2021-07-04 14:39:43 +0500

    Complete product's page

--- backend/parsers/views.py
+++ backend/parsers/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db import models
 from django.utils.decorators import method_decorator
 from django.views import generic
 from users.decorators import login_required
@@ -25,7 +26,14 @@ class ProductView(generic.DetailView):
             .annotate_price_with_vat()\
             .filter(price_with_vat__isnull=False)\
             .order_by("price_with_vat")
-        offers = Paginator(offers, self.offers_per_page).get_page(self.request.GET.get('page'))
-        context["offers"] = offers
+        offers_page = Paginator(offers, self.offers_per_page).get_page(self.request.GET.get('page'))
+        context["offers"] = offers_page
+
+        aggr = offers.aggregate(
+            min_price=models.Min("price_with_vat"),
+            avg_price=models.Avg("price_with_vat"),
+        )
+        context["min_price"] = aggr["min_price"]
+        context["avg_price"] = aggr["avg_price"]
 
         return context
