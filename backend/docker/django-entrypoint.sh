47a2c7a372b105f501a8e275510883a51c9cf5f9
Alexandr Vagner <sanyapalmero@gmail.com>
2021-07-04 16:48:22 +0500

    Upload users fixture

--- backend/docker/django-entrypoint.sh
+++ backend/docker/django-entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --no-input
 /app/docker/wait-for-it.sh ${POSTGRES_HOST:-db}:${POSTGRES_PORT:-5432}
 
 python manage.py migrate
-python manage.py loaddata providers products offers
+python manage.py loaddata providers products offers users
 
 tar -xzf /app/docker/screenshots.tar.gz -C /app/media/ screenshots
 
