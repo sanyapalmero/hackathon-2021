5f718eca77ea26ecad105431a2cddaf3d954cc5d
Oleg Kuznetsov <okuznetsov@iponweb.net>
2021-07-04 17:29:43 +0500

    Try to add some charts to templates

--- backend/static/ts/main.ts
+++ backend/static/ts/main.ts
@@ -2,9 +2,13 @@
 import '../scss/main.scss'
 
 import DemoFill from './demo';
-
+import ChartComponent from "./components/Chart";
 
 let demoFillButtons = [].slice.call(document.querySelectorAll('.DemoFill')) as HTMLButtonElement[];
 demoFillButtons.forEach(button => {
     new DemoFill(button);
 });
+
+window.document.querySelectorAll('.chart-component').forEach(element=>{
+    new ChartComponent(element as HTMLElement)
+})
