1c956b5140194aa26e3ad13143c27329cf684200
Alexandr Vagner <sanyapalmero@gmail.com>
2021-07-04 16:20:48 +0500

    Add deploy files

---
+++ backend/docker/django-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+
+python manage.py collectstatic --no-input
+
+/app/docker/wait-for-it.sh ${POSTGRES_HOST:-db}:${POSTGRES_PORT:-5432}
+
+python manage.py migrate
+python manage.py loaddata providers products offers
+
+tar -xzf /app/docker/screenshots.tar.gz -C /app/media/ screenshots
+
+exec "$@"
