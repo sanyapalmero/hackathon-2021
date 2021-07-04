404ebf29d16a45864af5b28191c2a1ef585ac843
Alexandr Vagner <sanyapalmero@gmail.com>
2021-07-04 16:43:46 +0500

    Add demo button with user fixture

--- backend/static/ts/main.ts
+++ backend/static/ts/main.ts
@@ -1,2 +1,10 @@
 // Styles
 import '../scss/main.scss'
+
+import DemoFill from './demo';
+
+
+let demoFillButtons = [].slice.call(document.querySelectorAll('.DemoFill')) as HTMLButtonElement[];
+demoFillButtons.forEach(button => {
+    new DemoFill(button);
+});
